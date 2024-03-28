de824ce2ea75d550f1518c342b7c438bfd3c63a1
Patrick <carbon2320@gmail.com>
2024-03-28 17:33:59 +0300

    server-0001 port forwarded over env vars

--- server/main.go
+++ server/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/patrick-me/game_one/game"
+	"go.uber.org/zap"
+	"os"
 )
 
+var logger *zap.Logger
+
+func init() {
+	logger, _ = zap.NewProduction()
+	defer logger.Sync()
+
+	if err := godotenv.Load(); err != nil {
+		logger.Info("No .env file found")
+	}
+}
+
 func main() {
+	defer logger.Sync()
 
 	world := &game.World{
 		IsServer: true,
@@ -21,6 +36,7 @@ func main() {
 			ServeWs(hub, world, c.Writer, c.Request)
 		}
 	}(hub, world))
-	ws.Run(":3000")
 
+	logger.Info("Listening on port: ", zap.String("port", os.Getenv("SERVER_PORT")))
+	ws.Run(":" + os.Getenv("SERVER_PORT"))
 }
